cff228efeced9dc0b5f0e55d6a79517f37f48e67
Adriel <adrieljpaul@outlook.com>
2025-01-14 11:16:32 +0530

    added code comments

--- test/checkinput_1/checkinput_1.ino
+++ test/checkinput_1/checkinput_1.ino
@@ -3,17 +3,21 @@ int raw[4];
 const int pin[4] = {32,33,34,35};
 void setup(){
   Serial.begin(115200);
-  Serial.println("DEBUG")
+  Serial.println("DEBUG");
 }
 void loop(){
   Serial.println();
   for(int i = 0; i<4;i++){
     raw[i] = analogRead(pin[i]);
     //Serial.println("Pin %d raw = %d",pin[i],raw[i]);
-    Serial.println("PIN = ");
-    Serial.print(pin[i]);
-    Serial.println("   RAW = ");
-    Serial.print(raw[i]);
+    //Serial.println("===PIN===");
+    //Serial.println(pin[i]);
+    //Serial.println("===RAW===");
+    //Serial.println(raw[i]);
+    Serial.printf("Pin %d raw = %d\n", pin[i], raw[i]);
+    Serial.println();
+
   } 
+  //cleanup for better debug readability
   delay(3000);
 }
