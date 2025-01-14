b7d57a4463cdef67069c95a6b1d9c44ceb03a058
Adriel <adrieljpaul@outlook.com>
2025-01-14 10:57:22 +0530

    so many errors while trying to process a formatted string. hate arduino for this but simplicity matters.

--- test/checkinput_1/checkinput_1.ino
+++ test/checkinput_1/checkinput_1.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+int raw[4];
 const int pin[4] = {32,33,34,35};
 void setup(){
   Serial.begin(115200);
@@ -7,10 +9,13 @@ void setup(){
 }
 void loop(){
   Serial.println();
-  int raw[4];
   for(int i = 0; i<4;i++){
     raw[i] = analogRead(pin[i]);
-    Serial.println("Pin %d raw = %d",pin[i],raw[i]);
+    //Serial.println("Pin %d raw = %d",pin[i],raw[i]);
+    Serial.println("PIN = ");
+    Serial.print(pin[i]);
+    Serial.print("   RAW = ");
+    Serial.print(raw[i]);
   } 
   delay(3000);
 }
